427c2bfad176b5917053882efbec7119dedbf6ba
sronglong <chhem.sronglong@gmail.com>
2022-09-29 16:19:34 +0200

    Test new changes
    - add share function to export data

--- packages/health/example/lib/main.dart
+++ packages/health/example/lib/main.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
+import 'dart:convert';
 import 'dart:math';
 
 import 'package:permission_handler/permission_handler.dart';
 
 import 'package:flutter/material.dart';
 import 'package:health/health.dart';
+import 'package:share_plus/share_plus.dart';
 
 void main() => runApp(HealthApp());
 
@@ -37,63 +39,37 @@ class _HealthAppState extends State<HealthApp> {
   Future fetchData() async {
     setState(() => _state = AppState.FETCHING_DATA);
 
-    // define the types to get
-    final types = [
-      HealthDataType.STEPS,
-      HealthDataType.WEIGHT,
-      HealthDataType.HEIGHT,
-      HealthDataType.BLOOD_GLUCOSE,
-      HealthDataType.WORKOUT,
-      // Uncomment these lines on iOS - only available on iOS
-      // HealthDataType.AUDIOGRAM
-    ];
+    final types = HealthFactory.allSupportedHealthDataTypes;
 
     // with coresponsing permissions
-    final permissions = [
-      HealthDataAccess.READ,
-      HealthDataAccess.READ,
-      HealthDataAccess.READ,
-      HealthDataAccess.READ,
-      HealthDataAccess.READ,
-      // HealthDataAccess.READ,
-    ];
+    final permissions = types.map((e) => HealthDataAccess.READ).toList();
 
     // get data within the last 24 hours
-    final now = DateTime.now();
-    final yesterday = now.subtract(Duration(days: 5));
+    final dateTo = DateTime.now();
+    final dateFrom = dateTo.subtract(Duration(days: 60));
     // requesting access to the data types before reading them
     // note that strictly speaking, the [permissions] are not
     // needed, since we only want READ access.
     bool requested =
         await health.requestAuthorization(types, permissions: permissions);
-    print('requested: $requested');
-
-    // If we are trying to read Step Count, Workout, Sleep or other data that requires
-    // the ACTIVITY_RECOGNITION permission, we need to request the permission first.
-    // This requires a special request authorization call.
     //
-    // The location permission is requested for Workouts using the Distance information.
+    // // If we are trying to read Step Count, Workout, Sleep or other data that requires
+    // // the ACTIVITY_RECOGNITION permission, we need to request the permission first.
+    // // This requires a special request authorization call.
+    // //
+    // // The location permission is requested for Workouts using the Distance information.
     await Permission.activityRecognition.request();
     await Permission.location.request();
-
+    // final requested = true;
     if (requested) {
       try {
         // fetch health data
         List<HealthDataPoint> healthData =
-            await health.getHealthDataFromTypes(yesterday, now, types);
-        // save all the new data points (only the first 100)
-        _healthDataList.addAll((healthData.length < 100)
-            ? healthData
-            : healthData.sublist(0, 100));
+            await health.getHealthDataFromTypes(dateFrom, dateTo, types);
+        _healthDataList = healthData;
       } catch (error) {
         print("Exception in getHealthDataFromTypes: $error");
       }
-
-      // filter out duplicates
-      _healthDataList = HealthFactory.removeDuplicates(_healthDataList);
-
-      // print the results
-      _healthDataList.forEach((x) => print(x));
 
       // update the UI to display the results
       setState(() {
@@ -311,6 +287,11 @@ class _HealthAppState extends State<HealthApp> {
     return _contentNotFetched();
   }
 
+  Future<void> _shareData()async {
+    final data = json.encode(_healthDataList);
+    await Share.share(data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -318,6 +299,12 @@ class _HealthAppState extends State<HealthApp> {
           appBar: AppBar(
             title: const Text('Health Example'),
             actions: <Widget>[
+              IconButton(
+                icon: Icon(Icons.share),
+                onPressed: () {
+                  _shareData();
+                },
+              ),
               IconButton(
                 icon: Icon(Icons.file_download),
                 onPressed: () {
